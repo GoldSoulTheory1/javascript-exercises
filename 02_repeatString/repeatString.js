cb92e6b10277a096987353afc68e070c7a6123cf
Jeff <ifbatmanwasreal@gmail.com>
2023-02-23 03:01:21 -0500

    solved 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,8 @@
-const repeatString = function() {
+function repeatString(string, num) {
+    return string.repeat(num);
+  }
 
-};
+console.log(repeatString("test", 5));
 
 // Do not edit below this line
 module.exports = repeatString;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return (fahrenheit - 32) * (5/9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return (celsius * (9/5)) + 32;
 };
+
+console.log(convertToCelsius(32)); // Output: 0
+console.log(convertToFahrenheit(0)); // Output: 32
+
 
 // Do not edit below this line
 module.exports = {
